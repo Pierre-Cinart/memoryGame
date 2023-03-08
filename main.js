ff73c5646bb167a618c6a2c87a40a2b03aadfc26
Pierre-Cinart <cinartpierre@gmail.com>
2023-03-08 15:35:30 +0100

    premiere mise en page

---
+++ main.js
@@ -0,0 +1,13 @@
+let hit = 0;
+let imgSources =  [
+    "./images/bear.png",
+    "./images.cat.png",
+    "./images/cow.png",
+    "./images/dog.png",
+    "./images/lezard.png",
+    "./images/monkey.png",
+    "./images/pig.png",
+    "./images/rabbit.png", 
+    "./images/rat.png"
+
+]
